08d43ef22e39ba943b1c70a869a1112003172613
karol-202 <karoljurski1@gmail.com>
2020-06-15 22:38:43 +0200

    Fix gh-pages

--- site/build.gradle.kts
+++ site/build.gradle.kts
@@ -1,17 +1,22 @@
 tasks {
-	val copyStatic by creating(Copy::class) {
-		from("static")
-		into("build/site")
+	val clean by creating(Delete::class) {
+		setDelete("build/site/")
 	}
 
 	val copyDocs by creating(Copy::class) {
 		dependsOn(":uranium-core:dokka")
-		from("../uranium-core/build/dokka/uranium-core")
-		into("build/site/docs")
+		from("../uranium-core/build/dokka/")
+		into("build/site/docs/")
+	}
+
+	val copyStatic by creating(Copy::class) {
+		from("static/")
+		into("build/site/")
 	}
 
 	val assembleSite by creating {
-		dependsOn(copyStatic)
+		dependsOn(clean)
 		dependsOn(copyDocs)
+		dependsOn(copyStatic)
 	}
 }
